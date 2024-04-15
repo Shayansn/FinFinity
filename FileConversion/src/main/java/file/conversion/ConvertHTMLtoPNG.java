505379d68d58034158e6c2f8b7d7f80125aa808f
Mohtashim Syed <128553217+mohtashim-syed@users.noreply.github.com>
2024-04-14 22:32:26 -0500

    Update ConvertHTMLtoPNG.java

--- FileConversion/src/main/java/file/conversion/ConvertHTMLtoPNG.java
+++ FileConversion/src/main/java/file/conversion/ConvertHTMLtoPNG.java
@@ -13,11 +13,13 @@ public class ConvertHTMLtoPNG {
         Document document = new Document();
 
         //Load a sample HTML file
-        document.loadFromFile("C:\\Users\\mohta\\Documents\\mus210002-workspace\\FinFinity\\FileConversion\\src\\main\\resources\\goalTracker.html", FileFormat.Html, XHTMLValidationType.None);
-
+        //document.loadFromFile("C:\\Users\\mohta\\Documents\\mus210002-workspace\\FinFinity\\FileConversion\\src\\main\\resources\\goalTracker.html", FileFormat.Html, XHTMLValidationType.None);
+        document.loadFromFile("SE Project/goalTracker.html", FileFormat.Html, XHTMLValidationType.None);
+        
         //Save to image. You can convert HTML to BMP, JPEG, PNG, GIF, Tiff etc.
         BufferedImage image= document.saveToImages(0, ImageType.Bitmap);
-        String result = "C:\\Users\\mohta\\Documents\\mus210002-workspace\\FinFinity\\FileConversion\\src\\main\\resources\\HtmlToImage.png";
+        //String result = "C:\\Users\\mohta\\Documents\\mus210002-workspace\\FinFinity\\FileConversion\\src\\main\\resources\\HtmlToImage.png";
+        String result = "FileConversion/src/main/resources/HtmlToImage.png"
         File file= new File(result);
         ImageIO.write(image, "PNG", file);
     }
